b4b829b7e213be9aad24ec22efcdc4f83074e222
Hager Khaled <78926069+hagerk720@users.noreply.github.com>
2023-04-16 15:08:04 +0200

    lab3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'lab3';
+  @Input("DataFromLogin") DataFromLogin :any ;
+  Students:{name:string, age:string}[] = [];
+
+  getData(data:any){
+    if(+data.age >=20 && +data.age <= 40 && data.name.length > 3 ) 
+    this.Students.push(data);
+    this.DataFromLogin = this.Students ;    
+  }
+
 }
